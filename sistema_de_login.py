9a29b9f3aefc1c87c48a1ca039068ceaae731a80
Edmilson Moraes <122231781+Edmilson-Moraes@users.noreply.github.com>
2026-03-26 06:33:31 -0300

    Refactor login attempt logic for clarity

--- sistema_de_login.py
+++ sistema_de_login.py
@@ -1,18 +1,15 @@
 # Sistema de Login com limite tentativas (3)
+senha_correta = '1234'          # ← variável simples (string)
+tentativas = 0                  # ← variável inteira que conta erros
 
-senha_correta = '1234'
-tentativas = 0
-
-while tentativas < 3:
-    senha = input("Digite a senha:")
-
-    if senha == senha_correta:
+while tentativas < 3:           # ← loop enquanto tentativas for menor que 3
+    senha = input("Digite a senha:")   # ← pede a senha do usuário
+    if senha == senha_correta:         # ← compara se acertou
         print("\nSenha correta.")
         print("\nBem vindo!")
-        break
+        break                          # ← sai do loop imediatamente
     else:
         print("\nSenha incorreta, tenta novamente!\n")
-    tentativas += 1
-
-    if tentativas == 3:
+    tentativas += 1                    # ← aumenta o contador de erro
+    if tentativas == 3:                # ← só depois de 3 erros mostra mensagem final
         print("\nfim das tentativas. Sistema bloqueado!")
